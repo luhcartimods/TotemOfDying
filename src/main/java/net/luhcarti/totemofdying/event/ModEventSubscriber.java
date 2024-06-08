047a91dbf927b3cac0ba760d9eb6848b4f3b935d
CrownSheep <106444061+CrownSheep@users.noreply.github.com>
2024-06-08 15:15:47 +0300

    Pillagers who spawn from the totem now spawn with crossbow, All totem spawned mobs will longer target the player while in creative mode

--- src/main/java/net/luhcarti/totemofdying/event/ModEventSubscriber.java
+++ src/main/java/net/luhcarti/totemofdying/event/ModEventSubscriber.java
@@ -6,16 +6,20 @@ import net.luhcarti.totemofdying.network.ModNetwork;
 import net.luhcarti.totemofdying.network.TotemActivationPacket;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.SpawnGroupData;
 import net.minecraft.world.entity.monster.Pillager;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -65,7 +69,10 @@ public class ModEventSubscriber {
                                 EntityType<?> mobType = mobTypes[level.random.nextInt(mobTypes.length)];
                                 Mob mob = (Mob) mobType.create(level);
                                 mob.setPos(player.getX() + level.random.nextDouble() * 10 - 5, player.getY(), player.getZ() + level.random.nextDouble() * 10 - 5);
-                                mob.setTarget(player);
+                                mob.setTarget(player.getAbilities().instabuild ? null : player);
+                                if(mob instanceof Pillager pillager) {
+                                    pillager.finalizeSpawn((ServerLevelAccessor) level, level.getCurrentDifficultyAt(player.getOnPos()), MobSpawnType.TRIGGERED, null, null);
+                                }
                                 level.addFreshEntity(mob);
                             }
                         } catch (InterruptedException e) {
