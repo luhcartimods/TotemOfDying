a9e66220be99f154264807164b9028d73838efcd
CrownSheep <106444061+CrownSheep@users.noreply.github.com>
2024-06-08 15:24:07 +0300

    Initial Commit

--- src/main/java/net/luhcarti/totemofdying/item/custom/ArchEvokerStaff.java
+++ src/main/java/net/luhcarti/totemofdying/item/custom/ArchEvokerStaff.java
@@ -1,36 +1,51 @@
 package net.luhcarti.totemofdying.item.custom;
 
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ArchEvokerStaff extends Item {
-    public ArchEvokerStaff(Properties pProperties) {
-        super(pProperties);
+    public ArchEvokerStaff(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        if (!world.isClient) {
+            BlockPos pos = user.getBlockPos().up();
+
+            Entity evokerFangs = EntityType.EVOKER_FANGS.create(world);
+            evokerFangs.refreshPositionAndAngles(pos.getX(), pos.getY(), pos.getZ(), 0, 0);
+            world.spawnEntity(evokerFangs);
+
+            Entity vex = EntityType.VEX.create(world);
+            vex.refreshPositionAndAngles(pos.getX(), pos.getY(), pos.getZ(), 0, 0);
+            world.spawnEntity(vex);
+        }
+        return super.use(world, user, hand);
     }
 
     //This just spawns a Evoker Fang and vex when used
-    @Override
-    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
-        if (!pLevel.isClientSide) {
-            BlockPos pos = pPlayer.blockPosition().above();
-
-            Entity evokerFangs = EntityType.EVOKER_FANGS.create(pLevel);
-            evokerFangs.moveTo(pos.getX(), pos.getY(), pos.getZ(), 0, 0);
-            pLevel.addFreshEntity(evokerFangs);
-
-            Entity vex = EntityType.VEX.create(pLevel);
-            vex.moveTo(pos.getX(), pos.getY(), pos.getZ(), 0, 0);
-            pLevel.addFreshEntity(vex);
-        }
-
-        return super.use(pLevel, pPlayer, pUsedHand);
-    }
+//    public InteractionResultHolder<ItemStack> use(Level pLevel, Player pPlayer, InteractionHand pUsedHand) {
+//        if (!pLevel.isClientSide) {
+//            BlockPos pos = pPlayer.blockPosition().above();
+//
+//            Entity evokerFangs = EntityType.EVOKER_FANGS.create(pLevel);
+//            evokerFangs.moveTo(pos.getX(), pos.getY(), pos.getZ(), 0, 0);
+//            pLevel.addFreshEntity(evokerFangs);
+//
+//            Entity vex = EntityType.VEX.create(pLevel);
+//            vex.moveTo(pos.getX(), pos.getY(), pos.getZ(), 0, 0);
+//            pLevel.addFreshEntity(vex);
+//        }
+//
+//        return super.use(pLevel, pPlayer, pUsedHand);
+//    }
 }
